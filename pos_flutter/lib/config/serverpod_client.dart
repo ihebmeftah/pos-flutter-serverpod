cb19b43e3fed21f3366cde7416641422fa086756
iheb meftah <ihebmeftah666@gmail.com>
2026-01-01 13:55:48 +0100

    Implement initial structure for Home module with bindings, controller, and view; add Serverpod client setup; remove unused screens; update dependencies.

---
+++ pos_flutter/lib/config/serverpod_client.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+import 'package:pos_client/pos_client.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
+import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
+import 'app_config.dart';
+
+class ServerpodClient extends GetxService {
+  static Client get instance => Get.find<ServerpodClient>()._client;
+  late Client _client;
+  late String serverUrl;
+
+  Future<ServerpodClient> initialize() async {
+    const serverUrlFromEnv = String.fromEnvironment('SERVER_URL');
+    final config = await AppConfig.loadConfig();
+    serverUrl = serverUrlFromEnv.isEmpty
+        ? config.apiUrl ?? 'http://$localhost:8080/'
+        : serverUrlFromEnv;
+    _client = Client('http://$localhost:8080/')
+      ..connectivityMonitor = FlutterConnectivityMonitor()
+      ..authSessionManager = FlutterAuthSessionManager();
+    await _client.auth.initialize();
+    return this;
+  }
+}
